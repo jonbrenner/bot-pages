a8275228c96b4a84c29f950ceebcccab8d1f0c4d
Jonathon Brenner <jonbrenner@gmail.com>
2023-03-29 10:28:20 -0400

    Don't wait on a keyMsg to quit.
    Quit immediately after the completion returns and is rendered.

--- bot.go
+++ bot.go
@@ -192,13 +192,9 @@ func (m Model) Init() tea.Cmd {
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		}
 	case Completion:
 		m.Completion = msg
+		return m, tea.Quit
 	}
 	return m, nil
 }
