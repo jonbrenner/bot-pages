ab4ae5ea1f64252203b499e3c7bc296e1477dff5
Sidney J. Sherrill <sisherri@cisco.com>
2023-07-27 09:36:58 -0400

    add -mode flag to original-command-structure, allowing for

--- bot.go
+++ bot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	_ "embed"
 	"flag"
 	"fmt"
@@ -11,31 +12,38 @@ import (
 	"sync"
 )
 
-//go:embed prompt_prefix.txt
-var promptPrefix string
+//go:embed prompt_prefix*.txt
+var promptPrefixFS embed.FS
 
 const promptStartText = "\n[EXAMPLES]\n"
+
+var err error
 
 func main() {
 
 	args := parseArgs()
-	if len(args.Prompt) == 0 {
+	if args.Usage || len(args.Prompt) == 0 {
 		usage()
 		os.Exit(1)
 	}
 
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error retrieving home directory: %v", err)
-		os.Exit(1)
+	var promptPrefix []byte
+	switch {
+	case args.Mode == "command":
+		promptPrefix, err = promptPrefixFS.ReadFile("prompt_prefix_command.txt")
+		errHandler(err, "Error reading prompt_prefix_command")
+	case args.Mode == "config":
+		promptPrefix, err = promptPrefixFS.ReadFile("prompt_prefix_config.txt")
+		errHandler(err, "Error reading prompt_prefix_config")
 	}
 
+	homeDir, err := os.UserHomeDir()
+	errHandler(err, "Error retrieving home directory")
+
+	// TODO: distinguish between config for bot program and config as a command flag
 	configPath := filepath.Join(homeDir, configFilename)
 	config, err := loadConfig(configPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading config: %v", err)
-		os.Exit(1)
-	}
+	errHandler(err, "Error loading config")
 
 	respCh := make(chan string)
 
@@ -45,7 +53,8 @@ func main() {
 	client := &OpenAIAdapter{APIKey: config.APIKey}
 	go func() {
 		defer wg.Done()
-		err := client.FetchCompletionStream(CreateRequest(args.Prompt), respCh)
+		err := client.FetchCompletionStream(CreateRequest(string(promptPrefix), args.Prompt), respCh)
+		errHandler(err)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v", err)
 			os.Exit(1)
@@ -61,19 +70,24 @@ func main() {
 }
 
 type Args struct {
+	Help        bool
 	Interactive bool
+	Mode        string
 	Prompt      string
 }
 
 func parseArgs() *Args {
+	help := flag.Bool("help", false, "Display usage options")
 	interactive := flag.Bool("i", false, "Enter interactive mode (optional)")
+	mode := flag.String("mode", "command", "Query config file details")
 	flag.Parse()
 	prompt := strings.Join(flag.Args(), " ")
-	return &Args{Interactive: *interactive, Prompt: prompt}
+	return &Args{Help: *help, Interactive: *interactive, Mode: *mode, Prompt: prompt}
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [prompt]", os.Args[0])
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
 }
 
 func RenderCompletionStreamResponse(w io.Writer, respCh <-chan string) {
@@ -81,4 +95,16 @@ func RenderCompletionStreamResponse(w io.Writer, respCh <-chan string) {
 		fmt.Fprint(w, token)
 	}
 	fmt.Fprintln(w, "")
+}
+
+func errHandler(err error, args ...string) {
+	errString := "Error"
+	if len(args) > 0 {
+		errString = args[0]
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v", errString, err)
+		os.Exit(1)
+	}
 }
